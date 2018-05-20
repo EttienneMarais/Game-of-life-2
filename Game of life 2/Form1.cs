edf19e09c19efc62c7609a47e2fcb0f594f7ff73
Ettienne Marais <Ettienne@gmail.com>
2018-05-20 22:02:45 +0200

    Add project files.

---
+++ Game of life 2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game_of_life_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeBoard();
+        }
+
+        private void InitializeBoard()
+        {
+            board.BackgroundColor = Color.Black;
+            board.DefaultCellStyle.BackColor = Color.Black;
+
+            for (int i = 0; i < 28; i++)
+                board.Rows.Add();
+            foreach (DataGridViewColumn c in board.Columns)
+                c.Width = board.Width / board.Columns.Count;
+
+        }
+
+
+    }
+}
